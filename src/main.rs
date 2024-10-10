96d4164c022ce7611c87cdca0577ffad49142706
Manan Arora <mananarorawork@gmail.com>
2024-10-10 20:52:10 +0530

    handling invalid input

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@ use std::{cmp::Ordering, io};
 use rand::Rng;
 
 fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..=100);
     loop {
-        let secret_number = rand::thread_rng().gen_range(1..=100);
-
     println!("Guess the number : ");
 
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("failed to read the line");
 
-    let guess : u32 = guess.trim().parse().expect("wrong input");
+    let guess : u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => continue ,
+    };
 
     println!("you guessed : {}" , guess);
 
