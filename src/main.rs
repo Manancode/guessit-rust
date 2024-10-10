d970bb63e2779e3d2e234ac6d388ea5df27df71e
Manan Arora <mananarorawork@gmail.com>
2024-10-10 20:05:39 +0530

    init guess

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use std::io;
+
+fn main() {
+    println!("Guess the number : ");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("failed to read the line");
+
+    println!("you guessed : {}" , guess);
+}
