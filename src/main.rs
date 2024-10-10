5caf5c5263395cad3d6279b0abd9d741250c006c
Manan Arora <mananarorawork@gmail.com>
2024-10-10 20:39:55 +0530

    match cmp()

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
-use std::io;
-
+use std::{cmp::Ordering, io};
 use rand::Rng;
 
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
+
     println!("the secret number is {}" , secret_number);
     println!("Guess the number : ");
+
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("failed to read the line");
 
+    let guess : u32 = guess.trim().parse().expect("wrong input");
+
     println!("you guessed : {}" , guess);
+
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("too small"),
+        Ordering::Equal => println!("you won"),
+        Ordering::Greater => println!("too big"),
+    }
 }
