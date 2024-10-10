4076415b4a3f354ef31238cd6e0fef1584767e7b
Manan Arora <mananarorawork@gmail.com>
2024-10-10 20:33:12 +0530

    init rand()

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::io;
 
+use rand::Rng;
+
 fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("the secret number is {}" , secret_number);
     println!("Guess the number : ");
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("failed to read the line");
